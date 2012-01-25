402ec3ca68a017e931c7ebbdbaa169ad94af0c1b
Christian Coomer <christiancoomer@gmail.com>
2012-01-25 06:29:26 -0800

    Only show debug info for development Rails environment

--- lib/booze/railtie.rb
+++ lib/booze/railtie.rb
@@ -1,10 +1,12 @@
 module Booze
   class Railtie < Rails::Railtie
     config.before_initialize do
-      require "booze/controller_mixins"
-      ActionController::Base.send(:include, Booze::ControllerMixins)
-      ActionController::Base.send(:before_filter, :clear_query_store)
-      ActionController::Base.send(:after_filter, :display_debug_bar)
+      if Rails.env.development?
+        require "booze/controller_mixins"
+        ActionController::Base.send(:include, Booze::ControllerMixins)
+        ActionController::Base.send(:before_filter, :clear_query_store)
+        ActionController::Base.send(:after_filter, :display_debug_bar)
+      end
     end
   end
 end

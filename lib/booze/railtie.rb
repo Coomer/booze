c936d9528a1f3b8e0d03e62cba5536dea2361879
Christian Coomer <christiancoomer@gmail.com>
2017-01-18 06:05:24 -0800

    Add comment and logging to railtie

--- lib/booze/railtie.rb
+++ lib/booze/railtie.rb
@@ -1,8 +1,16 @@
+# Railtie to run after Rails app has been initialized and everything in
+# config/initializers/ has been loaded.
+#
+# What we do is subscribe to Rails events (clear store on controller start/finish,
+# and save SQL when an ActiveRecord  query is executed). Then we create an
+# after_filter to get the queries from the query store and display them.
 module Booze
   class Railtie < Rails::Railtie
     config.after_initialize do
       debug_environments = Booze.config.debug_environments
+      Rails.logger.debug "booze: Inside railtie. Booze-enabled environments: #{Booze.config.debug_environments}. This environment: #{Rails.env}"
       if debug_environments.include?(Rails.env)
+        Rails.logger.debug "booze: Enabling now."
         # Once Rails starts processing the controller (or is done processing),
         # ensure the query store is empty
         ActiveSupport::Notifications.subscribe /(start_processing|process_action)\.action_controller/ do |name, start, finish, id, payload|
